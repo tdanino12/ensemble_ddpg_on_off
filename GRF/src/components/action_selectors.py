f4e007b637f7eb904e542da126503eece128f9c3
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2024-11-05 18:18:14 +0200

    Update action_selectors.py

--- GRF/src/components/action_selectors.py
+++ GRF/src/components/action_selectors.py
@@ -24,7 +24,10 @@ class MultinomialActionSelector():
         if test_mode and self.test_greedy:
             picked_actions = masked_policies.max(dim=2)[1]
         else:
+            #masked_policies = th.clamp(masked_policies, min=0)
+            masked_policies = masked_policies.float().cpu()
             picked_actions = Categorical(masked_policies).sample().long()
+            picked_actions = picked_actions.to(agent_inputs.device)
 
             # random_numbers = th.rand_like(agent_inputs[:, :, 0])
             # pick_random = (random_numbers < self.epsilon).long()
@@ -32,10 +35,46 @@ class MultinomialActionSelector():
             # picked_actions = pick_random * random_actions + (1 - pick_random) * picked_actions
 
         if not (th.gather(avail_actions, dim=2, index=picked_actions.unsqueeze(2)) > 0.99).all():
-            return self.select_action(agent_inputs, avail_actions, t_env, test_mode)
+            print("########################################################")
+            print("recursion")
+            print("########################################################")
+            return self.select_action_recursion(agent_inputs, avail_actions, t_env, test_mode)
 
         return picked_actions
 
+    def select_action_recursion(self, agent_inputs, avail_actions, t_env, test_mode=False):
+        # Assuming agent_inputs is a batch of Q-Values for each agent bav
+        self.epsilon = self.schedule.eval(t_env)
+
+        if test_mode:
+            # Greedy action selection only
+            self.epsilon = 0.0
+
+        # mask actions that are excluded from selection
+        masked_q_values = agent_inputs.clone()
+        masked_q_values[avail_actions == 0.0] = -float("inf")  # should never be selected!
+
+        random_numbers = th.rand_like(agent_inputs[:, :, 0])
+        pick_random = (random_numbers < self.epsilon).long()
+        
+        avail_actions_cpu = avail_actions.float().cpu()
+        random_actions = Categorical(avail_actions_cpu.float()).sample().long()
+        random_actions = random_actions.to(agent_inputs.device)
+        
+        picked_actions = pick_random * random_actions + (1 - pick_random) * masked_q_values.max(dim=2)[1]
+        if not (th.gather(avail_actions, dim=2, index=picked_actions.unsqueeze(2)) > 0.99).all():
+            print((th.gather(avail_actions, dim=2, index=random_actions.unsqueeze(2)) <= 0.99).squeeze())
+            print((th.gather(avail_actions, dim=2, index=masked_q_values.max(dim=2)[1].unsqueeze(2)) <= 0.99).squeeze())
+            print((th.gather(avail_actions, dim=2, index=picked_actions.unsqueeze(2)) <= 0.99).squeeze())
+
+            print('Action Selection Error')
+            # raise Exception
+            #return self.select_action(agent_inputs, avail_actions, t_env, test_mode)
+            try:
+                return self.select_action_recursion(agent_inputs, avail_actions, t_env, test_mode)
+            except RecursionError as e:
+                return self.select_action_recursion(agent_inputs, avail_actions, t_env, test_mode)
+        return picked_actions
 
 REGISTRY["multinomial"] = MultinomialActionSelector
 
