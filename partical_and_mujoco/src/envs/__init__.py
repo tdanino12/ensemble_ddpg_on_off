a6ab226fb62b2d7ab788124a2596df9bc7a0988d
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2024-11-06 16:37:51 +0200

    Update __init__.py

--- partical_and_mujoco/src/envs/__init__.py
+++ partical_and_mujoco/src/envs/__init__.py
@@ -9,6 +9,13 @@ def env_fn(env, **kwargs) -> MultiAgentEnv:
 REGISTRY = {}
 REGISTRY["sc2"] = partial(env_fn, env=StarCraft2Env)
 
+REGISTRY["cts_matrix_game"] = partial(env_fn, env=CtsMatrix)
+REGISTRY["particle"] = partial(env_fn, env=Particle)
+REGISTRY["mujoco_multi"] = partial(env_fn, env=MujocoMulti)
+REGISTRY["manyagent_swimmer"] = partial(env_fn, env=ManyAgentSwimmerEnv)
+REGISTRY["manyagent_ant"] = partial(env_fn, env=ManyAgentAntEnv)
+
+
 if sys.platform == "linux":
     os.environ.setdefault("SC2PATH",
                           os.path.join(os.getcwd(), "3rdparty", "StarCraftII"))
