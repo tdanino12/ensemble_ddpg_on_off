73e571e46e13276a2f0f604e1e8a80e25e341789
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2024-11-07 22:13:18 +0200

    Add files via upload

---
+++ partical_and_mujoco/src/envs/__init__.py
@@ -0,0 +1,17 @@
+from functools import partial
+from smac.env import MultiAgentEnv, StarCraft2Env
+from .particle import Particle
+import sys
+import os
+
+def env_fn(env, **kwargs) -> MultiAgentEnv:
+    return env(**kwargs)
+
+REGISTRY = {}
+REGISTRY["sc2"] = partial(env_fn, env=StarCraft2Env)
+REGISTRY["particle"] = partial(env_fn, env=Particle)
+
+
+if sys.platform == "linux":
+    os.environ.setdefault("SC2PATH",
+                          os.path.join(os.getcwd(), "3rdparty", "StarCraftII"))
