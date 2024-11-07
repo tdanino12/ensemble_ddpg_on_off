b7a18a2f07505c1e0ed2af374c44a0d298055913
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2024-11-07 22:06:18 +0200

    Delete partical_and_mujoco directory

--- partical_and_mujoco/src/modules/agents/__init__.py
+++
@@ -1,8 +0,0 @@
-REGISTRY = {}
-
-from .rnn_agent import RNNAgent
-from .grnn_agent import GRNNAgent
-from .depth_route_net import DepthRouteNet
-REGISTRY["rnn"] = RNNAgent
-REGISTRY["grnn"] = GRNNAgent
-REGISTRY["soft_new"] = DepthRouteNet
