3d779fe6be3be91ced51370732f2310891bfb94a
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2024-11-14 18:21:11 +0200

    Update pursuit.py

--- SMAC/src/envs/petting_zoo/pursuit.py
+++ SMAC/src/envs/petting_zoo/pursuit.py
@@ -45,7 +45,7 @@ class Pursuit(MultiAgentEnv):
 
     def get_obs_size(self):
         """ Returns the shape of the observation """
-        return len(self.observations['pursuer_{}'format(agent_id)])
+        return len(self.observations['pursuer_{}'.format(agent_id)])
 
     def get_state(self):
         return self.state
