7dc5343dddc90a21671bd7a433cea95cbe531c46
tdanino12 <36441678+tdanino12@users.noreply.github.com>
2026-02-05 11:58:06 +0200

    Update __init__.py

--- SMAC/src/envs/__init__.py
+++ SMAC/src/envs/__init__.py
@@ -4,6 +4,7 @@ from .stag_hunt import StagHunt
 from .petting_zoo import Pursuit
 from .lbforaging import ForagingEnv
 import sys
+from .macf import MultiAgentCar
 import os
 
 def env_fn(env, **kwargs) -> MultiAgentEnv:
@@ -14,7 +15,7 @@ REGISTRY["sc2"] = partial(env_fn, env=StarCraft2Env)
 REGISTRY["stag_hunt"] = partial(env_fn, env=StagHunt)
 REGISTRY["pursuit"] = partial(env_fn, env=Pursuit)
 REGISTRY["foraging"] =  partial(env_fn, env=ForagingEnv)
-
+REGISTRY["macf"] = partial(env_fn, env=MultiAgentCar)
 if sys.platform == "linux":
     os.environ.setdefault("SC2PATH",
                           os.path.join(os.getcwd(), "3rdparty", "StarCraftII"))
